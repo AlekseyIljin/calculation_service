57a4b7d738bbf2ad2b4ab4a2f0540825c3b5862f
Oleksii Ilin <master.iljin97@gmail.com>
2019-07-04 18:11:14 +0300

    done calculation

--- services/data_api.py
+++ services/data_api.py
@@ -6,11 +6,11 @@ class AccessToProjects:
         pass
 
     def get(self, id):
-        response = requests.get('https://localhost:<port>/projects/{}/calc'.format(id))
+        response = requests.get('https://localhost:<port>/projects/{0}/calc'.format(id))
         return response.json(), response.status_code
 
     def put(self, id, status):
-        requests.put('https://localhost:<port>/projects/{}/calc'.format(id), json={"status":status})
+        requests.put('https://localhost:<port>/projects/{0}/calc'.format(id), json={"status":status})
 
 
 class AccessToContracts:
@@ -19,5 +19,5 @@ class AccessToContracts:
         pass
 
     def get(self, id):
-        response = requests.get("http://localhost:5000/api/contracts/{}".format(id))
+        response = requests.get("http://localhost:5000/api/contracts/{0}".format(id))
         return response.json(), response.status_code
--- services/mission.py
+++ services/mission.py
@@ -5,16 +5,16 @@ from services import data_api
 from services.models import Calculation
 from .config import db
 
-celery = Celery('tasks', broker='amqp://guest@localhost:5672')
+celery = Celery('mission', broker='amqp://guest@localhost:5672')
 
 
 @celery.task
 def calculate_by_rules(project_id):
     calculation = Calculation()
-    project_info = data_api.AccessToProjects.get(project_id)
+    project_info = data_api.AccessToProjects.get(project_id)[0]
     data = project_info.get("data")
     contract = project_info.get("contract_id")
-    rules = data_api.AccessToContracts.get(contract)
+    rules = data_api.AccessToContracts.get(contract)[0]
     operands = []
     operator = rules.get('operator')
     for key in rules:
@@ -22,7 +22,8 @@ def calculate_by_rules(project_id):
         if name_field in data:
             operands.append(Decimal(data.get(name_field)))
 
-    calculate = Decimal(eval(str(operands[0]) + operator + str(operands[1]) + "*" + str(rules.get('coefficient'))))
+    expression = str(operands[0]) + operator + str(operands[1]) + "*" + str(rules.get('coefficient'))
+    calculate = Decimal(eval(expression))
     db.session.add(calculation)
     db.session.commit()
     data_api.AccessToProjects.put(project_id, 'completed')

6e7046356d4fe89e8b28f9638f4f0b5c77e6b101
Oleksii Ilin <master.iljin97@gmail.com>
2019-07-10 18:23:56 +0300

    done calculation

--- services/mission.py
+++ services/mission.py
@@ -5,16 +5,18 @@ from services import data_api
 from services.models import Calculation
 from .config import db
 
-celery = Celery('mission', broker='amqp://guest@localhost:5672')
+celery = Celery('mission', broker='amqp://guest:guest@localhost:5672')
+
 
 
 @celery.task
 def calculate_by_rules(project_id):
+
     calculation = Calculation()
-    project_info = data_api.AccessToProjects.get(project_id)[0]
+    project_info = data_api.AccessToProjects.get(project_id)
     data = project_info.get("data")
     contract = project_info.get("contract_id")
-    rules = data_api.AccessToContracts.get(contract)[0]
+    rules = data_api.AccessToContracts.get(contract)
     operands = []
     operator = rules.get('operator')
     for key in rules:
